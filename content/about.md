c64f785bb21fd7eefd29a86df13145099ba30c05
Joe Thel <jgraythel@gmail.com>
2022-03-11 23:19:29 -0800

    💬 🚧 add `about` page

---
+++ content/about.md
@@ -0,0 +1,15 @@
+---
+title: about
+---
+
+## me (computers)
+
+I'm a web engineer living in LA, currently working at [Deepgram](https://deepgram.com). I've worked in a variety of languages, but most recent professional experience is in the Elm, React, and Next/Node.js ecosystems. If you're looking to collaborate, please contact! In general I try to work in languages/problem spaces different from my professional ones. These days I'm hobbying in Rust and Haskell, but it's always subject to change.
+
+## me (music)
+
+I'm an oboist, composer, and occasional singer. My work in all three is focused on the weirdness of performance, and what it feels like to watch that kind of fall apart (e.g. breaking an instrument, playing music that's impossible to "get right", doing one thing over and over again, or very slowly)
+
+## the site
+
+This iteration of my personal site is pretty much entirely built in [Elm](https://elm-lang.org/), excluding the text-mapping used by the homepage/logo animation (animations themselves are elm-canvas). I highly recommend trying it out on something small-scale like this, which should just enough stretch your needs that you see what parts are easy and what parts are hard.
---
+++ src/Page/About.elm
@@ -0,0 +1,95 @@
+module Page.About exposing (Data, Model, Msg, page)
+
+import DataSource exposing (DataSource)
+import DataSource.File as File
+import Head
+import Head.Seo as Seo
+import Html exposing (Html)
+import Markdown.Parser
+import Markdown.Renderer
+import OptimizedDecoder as Decode exposing (Decoder)
+import Page exposing (Page, PageWithState, StaticPayload)
+import Pages.PageUrl exposing (PageUrl)
+import Pages.Url
+import Shared
+import View exposing (View)
+
+
+type alias Model =
+    ()
+
+
+type alias Msg =
+    Never
+
+
+type alias RouteParams =
+    {}
+
+
+type alias Data =
+    { title : String, body : List (Html.Html Msg) }
+
+
+markdownDecoder :
+    String
+    -> Decoder (List (Html.Html Msg))
+markdownDecoder =
+    Markdown.Parser.parse
+        >> Result.mapError
+            (List.map Markdown.Parser.deadEndToString >> String.join "\n")
+        >> Result.andThen
+            (Markdown.Renderer.render Markdown.Renderer.defaultHtmlRenderer)
+        >> Decode.fromResult
+
+
+content : DataSource Data
+content =
+    File.bodyWithFrontmatter
+        (\markdownString ->
+            Decode.map2 Data
+                (Decode.field "title" Decode.string)
+                (markdownDecoder markdownString)
+        )
+        "content/about.md"
+
+
+page : Page RouteParams Data
+page =
+    Page.single
+        { head = head
+        , data = content
+        }
+        |> Page.buildNoState { view = view }
+
+
+head :
+    StaticPayload Data RouteParams
+    -> List Head.Tag
+head _ =
+    -- TODO: replace boilerplate
+    Seo.summary
+        { canonicalUrlOverride = Nothing
+        , siteName = "elm-pages"
+        , image =
+            { url = Pages.Url.external "TODO"
+            , alt = "elm-pages logo"
+            , dimensions = Nothing
+            , mimeType = Nothing
+            }
+        , description = "TODO"
+        , locale = Nothing
+        , title = "TODO title"
+        }
+        |> Seo.website
+
+
+view :
+    Maybe PageUrl
+    -> Shared.Model
+    -> StaticPayload Data RouteParams
+    -> View Msg
+view _ _ static =
+    { title = static.data.title
+    , body = static.data.body
+    }
